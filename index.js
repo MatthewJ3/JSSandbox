be42bd2848b157a9bcad1aa38787178a98d4d809
MatthewJ3 <mevans.24@acsamman.edu.jo>
2022-04-18 19:19:45 -0700

    did work

---
+++ index.js
@@ -0,0 +1,31 @@
+
+<html lang="en">
+ <h1>Yo, world</h1>
+console.log("Yo, World!");
+ <h1>Variables and Types</h1>
+var myNumber=3;
+var myString="heeheehaha.";
+var myBoolean=true;
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
+ <h1>Arrays</h1>
+ var myArray = ["What's up brother?", 13, false];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+<h1>Manipulating Arrays</h1>
+var myArray = [true, "What is the meaning of life?"];
+myArray.push(23);
+myArray.shift(1);
+console.log(myArray[0]);
+console.log(myArray[1]);
+<h1>Operators</h1>
+var Age = currentyear - " " - birthyear;
+console.log("The name is " + Age);
+<h1>conditions</h1>
+Var checkAge(Age)
+if (CheckAge=Age<18)
+{console.log("Pathetic Child")}
+if (CheckAge=Age>18)
+{console.log("Filthy Adult")}
